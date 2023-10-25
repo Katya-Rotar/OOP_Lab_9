15c434782cae78286dbdc2c74c687d7381653875
user <katya.rotar02@gmail.com>
2023-10-25 21:33:58 +0300

    Problem 1. ListyIterator

---
+++ Task_1/ListyIterator.cs
@@ -0,0 +1,31 @@
+﻿class ListyIterator<T>{
+    private List<T> element;
+    private int currentIndex;
+
+    public ListyIterator(T[] element)
+    {
+        this.element = new List<T>(element);
+        this.currentIndex = 0;
+    }
+    public bool HasNext()
+    {
+        return currentIndex < element.Count - 1;
+    }
+    public bool Move() {
+        if(HasNext())
+        {
+            currentIndex++;
+            return true;
+        }
+        return false;
+    }
+    public void Print() {
+        if (element.Count == 0){
+            throw new InvalidOperationException("Invalid Operation!");
+        }
+        else
+        {
+            Console.WriteLine(element[currentIndex]);
+        }
+    }
+}
--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Create ");
+        string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        ListyIterator<string> listyIterator = new ListyIterator<string>(input);
+
+        while (true) {
+            try
+            {
+                string command = Console.ReadLine();
+                if (command.ToLower() == "end")
+                {
+                    break;
+                }
+                else
+                {
+                    switch (command)
+                    {
+                        case "Move":
+                            Console.WriteLine(listyIterator.Move());
+                            break;
+                        case "HasNext":
+                            Console.WriteLine(listyIterator.HasNext());
+                            break;
+                        case "Print":
+                            listyIterator.Print();
+                            break;
+                    }
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
