c2e504ed1cf60a1950727f78c266edaee1c1ce11
user <katya.rotar02@gmail.com>
2023-10-27 17:33:31 +0300

    Task_4_Problem_5

---
+++ Task_4_Problem_5/Person.cs
@@ -0,0 +1,27 @@
+﻿class Person: IComparable<Person> //тип T повинен реалізовувати інтерфейс IComparable<T>
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Town { get; set; }
+    public Person(string name, int age, string town) {
+        this.Name = name;
+        this.Age = age;
+        this.Town = town;
+    }
+    public int CompareTo(Person other)
+    {
+        int nameComparison = Name.CompareTo(other.Name);
+        if (nameComparison != 0)
+        {
+            return nameComparison;
+        }
+
+        int ageComparison = Age.CompareTo(other.Age);
+        if (ageComparison != 0)
+        {
+            return ageComparison;
+        }
+
+        return Town.CompareTo(other.Town);
+    }
+}
---
+++ Task_4_Problem_5/Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Person> people = new List<Person>();
+        while (true) {
+            string input = Console.ReadLine();
+            if (input.ToLower() == "end")
+            {
+                break;
+            }
+            else {
+                string[] arrayInput = input.Split(" ");
+                string name = arrayInput[0];
+                int age = int.Parse(arrayInput[1]);
+                string town = arrayInput[2];
+                Person person = new Person(name, age, town);
+                people.Add(person);
+            }
+        }
+        int n = int.Parse(Console.ReadLine());
+        if (n >= 1 && n <= people.Count)
+        {
+            Person targetPerson = people[n - 1];
+            int equalCount = 0;
+            int notEqualCount = 0;
+
+            foreach (Person person in people)
+            {
+                int comparison = targetPerson.CompareTo(person);
+                if (comparison == 0)
+                {
+                    equalCount++;
+                }
+                else
+                {
+                    notEqualCount++;
+                }
+            }
+
+            if (equalCount > 1)
+            {
+                Console.WriteLine($"{equalCount} {notEqualCount} {people.Count}");
+            }
+            else
+            {
+                Console.WriteLine("No matches");
+            }
+        }
+    }
+}
