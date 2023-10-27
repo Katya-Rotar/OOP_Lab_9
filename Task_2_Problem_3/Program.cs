a29a5aeffe3ee22926e6b800bb33b10749720faa
user <katya.rotar02@gmail.com>
2023-10-27 11:52:53 +0300

    Task_2_Problem_3

---
+++ Task_2_Problem_3/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Stack<int> stack = new Stack<int>();
+        while (true)
+        {
+            string command = Console.ReadLine();
+            if (command.ToLower() == "end")
+            {
+                break;
+            }
+            else {
+                string[] input = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (input[0].ToLower() == "push") {
+                    int[] convertArray = Array.ConvertAll(input[1].Split(' '), int.Parse);
+                    stack.Push(convertArray);
+                }
+                else if (input[0].ToLower() == "pop") {
+                    stack.Pop();
+                }
+            }
+        }
+        foreach(int i in stack)
+        {
+            Console.WriteLine(i);
+        }
+        foreach (int i in stack) {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Task_2_Problem_3/Stack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+class Stack<T> : IEnumerable<T>
+{
+    private List<T> stack;
+    public Stack() {
+        stack = new List<T>();
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = stack.Count - 1; i >= 0; i--) {
+            yield return stack[i];
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+    public void Push(T[] elements) {
+        foreach (T element in elements) {
+            stack.Add(element);
+        }
+    }
+    public void Pop() {
+        if (stack.Count > 0)
+        {
+            stack.RemoveAt(stack.Count - 1);
+        }
+        else
+        {
+            Console.WriteLine("No elements");
+        }
+    }
+}
