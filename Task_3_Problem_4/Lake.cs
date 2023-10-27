b9c9ecfadabfc6e5bd9bd9aaeb568a8e14a61b18
user <katya.rotar02@gmail.com>
2023-10-27 12:55:17 +0300

    Task_3_Problem_4

---
+++ Task_3_Problem_4/Lake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+class Lake<T> : IEnumerable<T>
+{
+    List<T> listStones;
+    public Lake(T[] values) {
+        listStones = new List<T>(values);
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < listStones.Count; i += 2) {
+            yield return listStones[i];
+        }
+        int lastIndex = listStones.Count % 2 == 0 ? listStones.Count -1: listStones.Count -2 ;
+        for (int i = lastIndex; i >= 0; i -= 2) {
+            yield return listStones[i];
+        }
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Task_3_Problem_4/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] input = Array.ConvertAll(Console.ReadLine().Split(", "), int.Parse);
+        Lake<int> lake = new Lake<int>(input);
+        Console.WriteLine("\n"+string.Join(", ", lake)); // об'єднання значень у певну послідовність використовуючи IEnumerable 
+    }
+}
